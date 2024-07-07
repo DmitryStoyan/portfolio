c6578bcfb1358ab1f7c58a0033321fd10e6245ec
DmitryStoyan <jombee@yandex.ru>
2024-07-07 23:46:51 +0300

    added animation on hero

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./hero.module.css";
 
 export const Hero = () => {
   const [descriptionText, setDescriptionText] = useState(
     "Дайте мне Visual Studio Code и Я rotate этот мир на (360deg)."
   );
+  const [showSubtitle, setShowSubtitle] = useState(false);
 
   const handleMouseEnter = () => {
     setDescriptionText("Дайте мне tasks, и Я will create чудо.");
@@ -16,11 +17,19 @@ export const Hero = () => {
     );
   };
 
+  useEffect(() => {
+    setShowSubtitle(true);
+  }, []);
+
   return (
     <section className={styles.hero}>
       <div className={`${styles.hero__wrapper} ${styles.hero__wrapperInfo}`}>
         <span className={styles.hero__messageTitle}>Hello, I'm</span>
-        <div className={styles.titleWrapper}>
+        <div
+          className={`${styles.titleWrapper} ${
+            showSubtitle ? styles.animateSubtitle : ""
+          }`}
+        >
           <h2 className={styles.hero__title}>Dmitry</h2>
           <h2 className={styles.hero__title}>Stoyan</h2>
         </div>
@@ -29,7 +38,9 @@ export const Hero = () => {
         className={`${styles.hero__wrapper} ${styles.hero__wrapperDescription}`}
       >
         <p
-          className={styles.hero__subtitle}
+          className={`${styles.hero__subtitle} ${
+            showSubtitle ? styles.animateSubtitle : ""
+          }`}
           onMouseEnter={handleMouseEnter}
           onMouseLeave={handleMouseLeave}
         >
